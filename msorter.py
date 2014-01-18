d802ca937f30bfe420c34fb99856603b5bdef233
Andrey <andrey.ivannikov@gmail.com>
2014-01-18 12:23:24 -0500

    Redesigned data structure

--- msorter.py
+++ msorter.py
@@ -3,33 +3,41 @@ Sort your music.  Call this in a directory with your music to get it sorted.
 """
 
 import os
+import sys
 import eyed3
 import tree
 
 import pdb
 
 music_collection = tree.Link('collection', 'my collection')
+songs = []
 
-for dirname, dirnames, filenames in os.walk('.'):
+path = sys.argv[1] if len(sys.argv) > 1 else '.'
+for dirname, dirnames, filenames in os.walk(path):
     for filename in filenames:
         song_full_path = os.path.join(dirname, filename)
         try:
             af = eyed3.load(song_full_path)
-            song_node = tree.Node(str(af.tag.title).title(),
-                                  str(af.tag.track_num[0]).rjust(2, '0'),
-                                  str(af.tag.album).title(),
-                                  song_full_path,
-                                  [str(af.tag.artist).title()])
-
-            artists[str(af.tag.artist).title()] = \
-                {'title': str(af.tag.title).title(), \
-                'track_num': str(af.tag.track_num[0]).rjust(2, '0'), \
-                'filepath': song_full_path}
-            # This ^ does not work, need a data structure containing albums
+            songs.append({'title': str(af.tag.title).title(),
+                'album': str(af.tag.album).title(),
+                'artist': str(af.tag.artist).title(),
+                'track_num': af.tag.track_num,
+                'full_path': str(song_full_path)})
 
         except AttributeError:
-            print('Got attribute error')
+            print('Got attribute error for song: ' + str(song_full_path))
             pass
 
 
-pdb.set_trace()
+albums = {}
+for song in songs:
+    try:
+        albums[song['album']].append(song)
+    except KeyError:
+        print('No album')
+        albums[song['album']] = [song]
+
+for album in albums:
+    print(str(album) + ':')
+    for song in albums[album]:
+        print('    ' + str(song['artist']))
